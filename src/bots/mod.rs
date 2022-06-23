555f045873c8b9f4f045475484d311412d7fd0c3
Forest Anderson <forestkzanderson@gmail.com>
2022-06-23 10:02:06 -0400

    Fixing scoring

--- src/bots/mod.rs
+++ src/bots/mod.rs
@@ -33,7 +33,7 @@ pub enum Dilemma {
     Betray,
 }
 
-#[derive(Debug, EnumIter, Eq, Hash, PartialEq, Clone)]
+#[derive(Debug, EnumIter, Eq, Hash, PartialEq, Clone, Copy)]
 pub enum Bots {
     AngelOnFira,
     FiftyFifty,
@@ -62,65 +62,68 @@ impl Simulation {
     pub fn run(&self) -> HashMap<Bots, i32> {
         let mut results: HashMap<Bots, i32> = HashMap::new();
 
-        // Simulate each bot fighting against each other bot 1 million times
-        for _ in 0..1_000 {
-            for (i, bot_1_type) in Bots::iter().enumerate() {
-                let mut new_bot_1 = bot_1_type.objects();
-                for bot_2_type in Bots::iter().skip(i + 1) {
-                    let mut new_bot_2 = bot_2_type.objects();
-
-                    let mut bot1_score = 0;
-                    let mut bot2_score = 0;
-
-                    let mut bot_1_history = Vec::new();
-                    let mut bot_2_history = Vec::new();
-
-                    // Simulate 1000 fights between bot1 and bot2
-                    for _ in 0..1_000 {
-                        let bot1_dilemma = new_bot_1.turn(&bot_1_history);
-                        let bot2_dilemma = new_bot_2.turn(&bot_2_history);
-
-                        // Add the histories
-                        bot_1_history.push(Turn {
-                            you: bot1_dilemma,
-                            other_bot: bot2_dilemma,
-                        });
-
-                        bot_2_history.push(Turn {
-                            you: bot2_dilemma,
-                            other_bot: bot1_dilemma,
-                        });
-
-                        // Determine the outcome of the fight
-                        match (bot1_dilemma, bot2_dilemma) {
-                            (Dilemma::Silence, Dilemma::Silence) => {
-                                bot1_score += 3;
-                                bot2_score += 3;
-                            }
-                            (Dilemma::Silence, Dilemma::Betray) => {
-                                bot2_score += 5;
-                            }
-                            (Dilemma::Betray, Dilemma::Silence) => {
-                                bot1_score += 5;
-                            }
-                            (Dilemma::Betray, Dilemma::Betray) => {
-                                bot1_score += 1;
-                                bot2_score += 1;
-                            }
-                        }
-                    }
-
-                    // Add the score to the results
-                    results
-                        .entry(bot_1_type.clone())
-                        .and_modify(|e| *e += bot1_score)
-                        .or_insert(bot1_score);
-
-                    results
-                        .entry(bot_2_type)
-                        .and_modify(|e| *e += bot2_score)
-                        .or_insert(bot2_score);
+        // Simulate each bot fighting against each other
+        for bot_1_type in Bots::iter() {
+            let mut new_bot_1 = bot_1_type.objects();
+            for bot_2_type in Bots::iter() {
+                // Prevent bots from fighting against themselves
+                if bot_1_type == bot_2_type {
+                    continue;
                 }
+
+                let mut new_bot_2 = bot_2_type.objects();
+
+                let mut bot_1_history = Vec::new();
+                let mut bot_2_history = Vec::new();
+
+                // Simulate 1000 fights between bot1 and bot2
+                for _ in 0..1_000 {
+                    let bot1_dilemma = new_bot_1.turn(&bot_1_history);
+                    let bot2_dilemma = new_bot_2.turn(&bot_2_history);
+
+                    // Add the histories
+                    bot_1_history.push(Turn {
+                        you: bot1_dilemma,
+                        other_bot: bot2_dilemma,
+                    });
+
+                    bot_2_history.push(Turn {
+                        you: bot2_dilemma,
+                        other_bot: bot1_dilemma,
+                    });
+                }
+
+                // Calculate the scores of both bots
+                let (bot_1_score, bot_2_score) =
+                    bot_1_history
+                        .iter()
+                        .fold((0, 0), |(bot_1_score, bot_2_score), turn| {
+                            match (turn.you, turn.other_bot) {
+                                (Dilemma::Silence, Dilemma::Silence) => {
+                                    (bot_1_score + 3, bot_2_score + 3)
+                                }
+                                (Dilemma::Silence, Dilemma::Betray) => {
+                                    (bot_1_score, bot_2_score + 5)
+                                }
+                                (Dilemma::Betray, Dilemma::Silence) => {
+                                    (bot_1_score + 5, bot_2_score)
+                                }
+                                (Dilemma::Betray, Dilemma::Betray) => {
+                                    (bot_1_score + 1, bot_2_score + 1)
+                                }
+                            }
+                        });
+
+                // Add the score to the results
+                results
+                    .entry(bot_1_type.clone())
+                    .and_modify(|e| *e += bot_1_score)
+                    .or_insert(bot_1_score);
+
+                results
+                    .entry(bot_2_type)
+                    .and_modify(|e| *e += bot_2_score)
+                    .or_insert(bot_2_score);
             }
         }
 
